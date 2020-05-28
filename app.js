66b323033d3006689309bc5a65c7ea129f01a8ec
unknown <andresfeijoo87@gmail.com>
2020-05-28 02:24:34 -0300

    valida nombre, apellido, edad, comentario y correo electronico

---
+++ app.js
@@ -0,0 +1,42 @@
+var correo;
+var expresion;
+
+function valida_envia(){
+   	if (document.registro.nombre.value.length<3){
+      		var errornombr = document.getElementById("errornombre");
+			errornombr.innerHTML = "Tiene que escribir su nombre"
+			console.log("Tiene que escribir su nombre")
+			return 0;
+      		}
+	if (document.registro.apellido.value.length<3){
+      		var errorapellid = document.getElementById("errorapellido");
+			errorapellid.innerHTML = "Tiene que escribir su apellido"
+			console.log("Tiene que escribir su apellido")
+			return 0;
+      		}
+  	edad = document.registro.edad.value
+   	if (edad==""){
+      		var erroreda = document.getElementById("erroredad");
+			erroreda.innerHTML = "Tiene que introducir un número mayor que cero."
+			console.log("Tiene que introducir un número mayor que cero.")
+      		document.registro.edad.focus()
+      		return 0;
+   	}
+	correo=document.getElementById("email").value;
+	expresion= /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+	if (!expresion.test(email.value)){ 
+			document.getElementById("erroremail").innerHTML = "Ingrese un Email valido";
+			console.log("Ingrese un Email valido")        
+			return false
+	}
+	if (document.registro.coment.value.length<20){
+      		var errorcomentari = document.getElementById("errorcoment");
+			errorcomentari.innerHTML = "Tiene que escribir al menos 20 letras"
+			console.log("Tiene que escribir al menos 20 letras")
+			return 0;
+      		}
+
+	alert("Muchas gracias por enviar el formulario");
+
+   
+}	
