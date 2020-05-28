4b7d26c26db94b790c80c3ad2e5a60bc3e2915d2
unknown <andresfeijoo87@gmail.com>
2020-05-28 02:37:49 -0300

    muestra algunos datos en consola

--- app.js
+++ app.js
@@ -14,14 +14,6 @@ function valida_envia(){
 			console.log("Tiene que escribir su apellido")
 			return 0;
       		}
-  	edad = document.registro.edad.value
-   	if (edad==""){
-      		var erroreda = document.getElementById("erroredad");
-			erroreda.innerHTML = "Tiene que introducir un número mayor que cero."
-			console.log("Tiene que introducir un número mayor que cero.")
-      		document.registro.edad.focus()
-      		return 0;
-   	}
 	correo=document.getElementById("email").value;
 	expresion= /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
 	if (!expresion.test(email.value)){ 
@@ -29,13 +21,25 @@ function valida_envia(){
 			console.log("Ingrese un Email valido")        
 			return false
 	}
+	  	edad = document.registro.edad.value
+   	if (edad==""){
+      		var erroreda = document.getElementById("erroredad");
+			erroreda.innerHTML = "Tiene que introducir un número mayor que cero."
+			console.log("Tiene que introducir un número mayor que cero.")
+      		document.registro.edad.focus()
+      		return 0;
+	}
 	if (document.registro.coment.value.length<20){
       		var errorcomentari = document.getElementById("errorcoment");
 			errorcomentari.innerHTML = "Tiene que escribir al menos 20 letras"
 			console.log("Tiene que escribir al menos 20 letras")
 			return 0;
       		}
-
+	console.log(document.registro.nombre.value)
+	console.log(document.registro.apellido.value)
+	console.log(document.getElementById("email").value)
+	console.log(document.registro.edad.value)
+	console.log(document.registro.coment.value)
 	alert("Muchas gracias por enviar el formulario");
 
    
